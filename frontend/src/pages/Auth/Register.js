e80f7a35beb506fa77b99cad4f3cc46ac453c8c3
Julio-Lauwers <juliolauwerss@gmail.com>
2024-03-08 19:41:33 -0300

    Criando formulario de cadastro

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,9 +1,31 @@
 import "./Auth.css"
 
+//components
+import {Link} from 'react-router-dom'
+
+// Hooks
+import { useState, useEffect } from "react"
+
 const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div>
-      Register
+      <h2>File Manager</h2>
+      <p className="subtitle">Cadastre-se para organizar seus arquivos</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a Senha" />
+        <input type="submit" value="Cadastrar"/>
+      </form>
+      <p>
+        Já possui conta? <Link to="/login">Clique aqui</Link>
+      </p>
     </div>
   )
 }
